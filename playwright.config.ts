8c0710e9407f26d7a9a6953b320670bea01b2021
aliaksandr-tsimoshyn <aliaksandr.tsimoshyn@gmail.com>
2023-11-16 13:01:22 +0300

    path

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,9 @@ import dotenv from 'dotenv'
 import path from 'path';
 
 dotenv.config({
-  path: `./envs/.env.${process.env.ENV || 'qa'}`
+  //path: `./envs/.env.${process.env.ENV || 'qa'}`
+  //path: `./envs/.env${process.env.ENV ? '.' + process.env.ENV : ''}`
+  //path: './envs/.env' + (process.env.ENV ? '.' + process.env.ENV : '')
   //terminal: $env:ENV="prod"  
 })
 console.log(process.env.ENV, process.env.URL)
