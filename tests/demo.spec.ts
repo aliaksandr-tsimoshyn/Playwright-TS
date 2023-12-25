b5607d7866e6379e82f5dc4deb93ed38c93bccd0
aliaksandr-tsimoshyn <aliaksandr.tsimoshyn@gmail.com>
2023-12-25 19:01:27 +0300

    demo tests update

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -12,7 +12,7 @@ test.describe(`Resource 1`, () => {
   test(`@demo Client Side Delay`, async ({ page }) => {
     await page.goto(`http://uitestingplayground.com/clientdelay`)
     await page.getByText(`Button Triggering Client Side Logic`).click()
-    const loadedData = page.locator(`.bg-success`)
+    let loadedData = page.locator(`.bg-success`)
     expect(loadedData, `Data isn't loaded`).toBeVisible()
     await loadedData.click()
   })
@@ -20,22 +20,48 @@ test.describe(`Resource 1`, () => {
   test(`@demo Progress Bar`, async ({ page }) => {
     await page.goto(`http://uitestingplayground.com/progressbar`)
     await page.locator(`#startButton`).click()
-    //await page.locator(`[aria-valuenow='75']`).isEnabled()
-    await expect(page.locator(`#progressBar`)).toContainText("75")
+    await page.locator(`[aria-valuenow='75']`).isEnabled()
     await page.locator(`#stopButton`).click()
+    let result = await page.locator(`#result`).textContent()
+    console.log(`${result}`)
   })
 
   test(`@demo Shadow Dom`, async ({ context }) => {
     await context.grantPermissions(["clipboard-read", "clipboard-write"])
-    const page = await context.newPage()
+    let page = await context.newPage()
     await page.goto(`http://uitestingplayground.com/shadowdom`)
     await page.locator(`#buttonGenerate`).click()
-    const generatedValue = await page.locator(`#editField`).inputValue()
+    let generatedValue = await page.locator(`#editField`).inputValue()
     expect(generatedValue).toHaveLength(36)
     await page.locator(`#buttonCopy`).click()
-    const clipboardContent = await page.evaluate(() =>
+    let clipboardContent = await page.evaluate(() =>
       navigator.clipboard.readText()
     )
     expect(generatedValue, `Values aren't equal`).toEqual(clipboardContent)
+  })
+})
+
+test.describe(`Resource 2`, () => {
+  test.describe.configure({ mode: `default` })
+
+  test(`@demo Drag and Drop`, async ({ page }) => {
+    await page.goto(`https://demoqa.com/dragabble`)
+    await page.dragAndDrop(
+      `#dragBox`,
+      `#draggableExample-tab-containerRestriction`
+    )
+  })
+})
+
+test.describe(`Resource 3`, () => {
+  test.describe.configure({ mode: `default` })
+
+  test(`@demo iFrame`, async ({ page }) => {
+    await page.goto(`https://the-internet.herokuapp.com/iframe`)
+    await page.getByText(`File`).click()
+    await page.getByText(`New document`).click()
+    let frameInput = page.frameLocator(`#mce_0_ifr`).locator(`#tinymce`)
+    await frameInput.fill(`Demo text`)
+    expect(frameInput, `Text isn't written`).toContainText(`Demo text`)
   })
 })
